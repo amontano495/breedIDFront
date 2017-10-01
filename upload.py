192f5bb179d70a39b58584218d2a4b11f51dc51b
Adam <amontano495@gmail.com>
2017-09-30 17:44:59 -0700

    new file:   static/default.jpg 	modified:   templates/upload.html 	modified:   upload.py 	deleted:    static/dog.jpeg 	deleted:    templates/uploaded.html

--- upload.py
+++ upload.py
@@ -4,17 +4,15 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = './static'
 
-@app.route('/upload')
-def upload():
-   return render_template('upload.html')
-
 @app.route('/uploader', methods = ['GET','POST'])
 def upload_file():
+   if request.method == 'GET':
+       return render_template('upload.html',imgname='default.jpg')
    if request.method == 'POST':
       f = request.files['file']
       sfname = 'static/'+str(secure_filename(f.filename))
       f.save(sfname)
-      return render_template('uploaded.html',imgname=f.filename)
+      return render_template('upload.html',imgname=f.filename)
 
 if __name__ == '__main__':
    app.run(debug = True)
